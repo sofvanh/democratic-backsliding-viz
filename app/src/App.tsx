85f99a72f3b6f108701992831d06de272b8199b5
sofvanh <23138848+sofvanh@users.noreply.github.com>
2024-05-02 12:08:56 +0300

    Show graph!!

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,23 +1,80 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { ResponsiveLine } from '@nivo/line';
+import rawData from './prod-dataset.json';
 
 function App() {
+
+  const filteredData = rawData.filter(serie => serie.id.startsWith('Afghanistan'));
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{ height: 400, width: 800 }}>
+          Democratic development in Afghanistan
+          <ResponsiveLine
+            data={filteredData}
+            margin={{ top: 50, right: 170, bottom: 50, left: 60 }}
+            xScale={{ type: 'point' }}
+            yScale={{ type: 'linear', min: 'auto', max: 'auto', stacked: true, reverse: false }}
+            axisTop={null}
+            axisRight={null}
+            axisBottom={{
+              // TODO Make tickValues dynamic to screen/graph width
+              tickValues: [1800, 1825, 1850, 1875, 1900, 1925, 1950, 1975, 2000],
+              tickSize: 5,
+              tickPadding: 5,
+              tickRotation: 0,
+              legend: 'Year',
+              legendOffset: 36,
+              legendPosition: 'middle'
+            }}
+            axisLeft={{
+              tickSize: 5,
+              tickPadding: 5,
+              tickRotation: 0,
+              legend: 'Score',
+              legendOffset: -40,
+              legendPosition: 'middle'
+            }}
+            colors={{ scheme: 'nivo' }}
+            pointSize={1}
+            pointColor={{ theme: 'background' }}
+            pointBorderWidth={2}
+            pointBorderColor={{ from: 'serieColor' }}
+            pointLabel="y"
+            pointLabelYOffset={-12}
+            useMesh={true}
+            enableGridX={false}
+            enableGridY={false}
+            legends={[
+              {
+                anchor: 'bottom-right',
+                direction: 'column',
+                justify: false,
+                translateX: 170,
+                translateY: 0,
+                itemsSpacing: 0,
+                itemDirection: 'left-to-right',
+                itemWidth: 160,
+                itemHeight: 20,
+                itemOpacity: 0.75,
+                symbolSize: 12,
+                symbolShape: 'circle',
+                symbolBorderColor: 'rgba(0, 0, 0, .5)',
+                effects: [
+                  {
+                    on: 'hover',
+                    style: {
+                      itemBackground: 'rgba(0, 0, 0, .03)',
+                      itemOpacity: 1
+                    }
+                  }
+                ]
+              }
+            ]}
+          />
+        </div>
       </header>
     </div>
   );
--- create-prod-dataset.py
+++ create-prod-dataset.py
@@ -1,8 +1,26 @@
-# Read the V-Dem-CY-Core-v14 dataset, get the relevant data, and save as json
+# Read the V-Dem-CY-Core-v14 dataset, get the relevant data, and save as json suitable for Nivo
+
+# TODO Why does all data from before 1900 disappear?!
 
 import pandas as pd
+import json
 
 df = pd.read_csv('V-Dem-CY-Core-v14.csv')
 df = df[['country_name', 'year', 'v2x_polyarchy', 'v2x_libdem', 'v2x_partipdem', 'v2x_delibdem', 'v2x_egaldem']]
-df.to_json('prod-dataset.json', orient='records')
+df = df[df.groupby('country_name')['year'].transform(max) == 2023]
+df = df.dropna()
 
+result = []
+indices = ['v2x_polyarchy', 'v2x_libdem', 'v2x_partipdem', 'v2x_delibdem', 'v2x_egaldem']
+grouped = df.groupby('country_name')
+
+for country, group in grouped:
+    for index in indices:
+        series_data = {
+            'id': f"{country}_{index}",
+            'data': [{'x': row['year'], 'y': row[index]} for _, row in group.iterrows()]
+        }
+        result.append(series_data)
+
+with open('prod-dataset.json', 'w') as f:
+    json.dump(result, f)
